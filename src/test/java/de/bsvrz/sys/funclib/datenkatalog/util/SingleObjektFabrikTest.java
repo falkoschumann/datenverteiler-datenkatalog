6a5af0e8d2066adab4b1d0cc049850bd65fb6c8c
Falko Schumann <falko.schumann@muspellheim.de>
2015-07-16 21:34:04 +0200

    Closes #21 Hilfsklasse zur Bestimmung eines einzigen Objekts

---
+++ src/test/java/de/bsvrz/sys/funclib/datenkatalog/util/SingleObjektFabrikTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2015 Falko Schumann
+ * Released under the terms of the MIT License.
+ */
+
+package de.bsvrz.sys.funclib.datenkatalog.util;
+
+import de.bsvrz.dav.daf.main.config.SystemObject;
+import de.bsvrz.sys.funclib.datenkatalog.AbstractDatenkatalogIT;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class SingleObjektFabrikTest extends AbstractDatenkatalogIT {
+
+    private SingleObjektFabrik fabrik;
+
+    @Before
+    public void setUp() {
+        fabrik = new SingleObjektFabrik(getModel());
+    }
+
+    @Test
+    public void testEinzigesObjektFinden() {
+        SystemObject aoe = fabrik.bestimmeObjekt("typ.autarkeOrganisationsEinheit", "-aoe=");
+        assertNotNull(aoe);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testMehrereObjekteGefunden_AusnahmeWerfen() {
+        fabrik.bestimmeObjekt("typ.messQuerschnittAllgemein", "-mq=");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testKeinObjektGefunden_AusnahmeWerfen() {
+        fabrik.bestimmeObjekt("typ.straße", "-mq=");
+    }
+
+    public void testObjektMitPid() {
+        SystemObject mq = fabrik.bestimmeObjekt("typ.messQuerschnittAllgemein", "mq.a10.0001", "-mq=");
+        assertNotNull(mq);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPidUnbekannt_AusnahmeWerfen() {
+        fabrik.bestimmeObjekt("typ.messQuerschnittAllgemein", "mq.foo", "-mq=");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testTypUndPidPassenNichtZusammen_AusnahmeWerfen() {
+        fabrik.bestimmeObjekt("typ.autarkeOrganisationsEinheit", "mq.a10.0001", "-aoe=");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testTypUnbekannt_AusnahmeWerfen() {
+        fabrik.bestimmeObjekt("typ.foo", "-foo=");
+    }
+
+}
