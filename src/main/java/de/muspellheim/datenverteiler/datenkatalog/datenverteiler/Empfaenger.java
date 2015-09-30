35460533c261d97bc0ab94610ffde84e73fd866a
Falko Schumann <falko.schumann@muspellheim.de>
2015-09-30 11:28:30 +0200

    Empfängerthread entfernt
    Die Entkopplung zwischen Updaterthread und Applikation liegt jetzt in
    der Verantwortung der Applikation.

--- src/main/java/de/muspellheim/datenverteiler/datenkatalog/datenverteiler/Empfaenger.java
+++ src/main/java/de/muspellheim/datenverteiler/datenkatalog/datenverteiler/Empfaenger.java
@@ -12,18 +12,12 @@ import de.muspellheim.datenverteiler.datenkatalog.bind.Context;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.function.Consumer;
 
 /**
  * Empfängt einen Onlinedatensatz eines bestimmten Typs und verteilt den Datensatz weiter.
- *
- * <p>Um die Verteilung der Datensätze durch die Datenverteiler-Applikationsfunktionen nicht zu blockieren, werden die
- * empfangenen Datensätze in einer Warteschlange eingereiht und in einem eigenen Thread weiterverarbeitet.</p>
  *
  * @param <T> der Typ der empfangenen Daten.
  * @author Falko Schumann
@@ -32,21 +26,16 @@ import java.util.function.Consumer;
 public class Empfaenger<T> implements ClientReceiverInterface {
 
     private final List<Consumer<Datensatz<T>>> consumers = new CopyOnWriteArrayList<>();
-    private final BlockingQueue<ResultData> warteschlange = new LinkedBlockingQueue<>();
 
     private final Context context;
-    private Empfaengeranmeldung<T> anmeldung;
+    private Class<T> datumTyp;
 
     /**
      * Initialisiert den Empfänger mit dem Kontext des Databindings und des Typs der Daten.
      */
-    public Empfaenger(Context context, Empfaengeranmeldung<T> anmeldung) {
+    public Empfaenger(Context context, Class<T> datumTyp) {
         this.context = context;
-        this.anmeldung = anmeldung;
-
-        Thread t = new Thread(this::veroeffentlicheNeueDatensaetze, "Empfänger für " + anmeldung);
-        t.setDaemon(true);
-        t.start();
+        this.datumTyp = datumTyp;
     }
 
     /**
@@ -63,24 +52,22 @@ public class Empfaenger<T> implements ClientReceiverInterface {
         consumers.remove(c);
     }
 
-    @Override
-    public void update(ResultData[] results) {
-        warteschlange.addAll(Arrays.asList(results));
+    /**
+     * Prüft, ob der Empfänger noch Verbraucher kennt.
+     */
+    public boolean hasConsumer() {
+        return consumers.isEmpty();
     }
 
-    private void veroeffentlicheNeueDatensaetze() {
-        while (true) {
-            try {
-                ResultData rd = warteschlange.take();
-                consumers.stream().forEach(c -> veroeffentlicheDatensatz(c, rd));
-            } catch (InterruptedException ex) {
-                break;
-            }
-        }
+    @Override
+    public void update(ResultData[] results) {
+        for (ResultData rd : results)
+            for (Consumer<Datensatz<T>> c : consumers)
+                veroeffentlicheDatensatz(c, rd);
     }
 
     private void veroeffentlicheDatensatz(Consumer<Datensatz<T>> c, ResultData rd) {
-        T datum = context.createUnmarshaller().unmarshal(rd.getData(), anmeldung.getDatumTyp());
+        T datum = context.createUnmarshaller().unmarshal(rd.getData(), datumTyp);
         LocalDateTime zeitstempel = LocalDateTime.ofInstant(Instant.ofEpochMilli(rd.getDataTime()), ZoneId.systemDefault());
         Datensatz<T> datensatz = Datensatz.of(rd.getObject(), datum, rd.getDataDescription().getAspect(), zeitstempel);
         c.accept(datensatz);
--- src/main/java/de/muspellheim/datenverteiler/datenkatalog/datenverteiler/Empfaengeranmeldung.java
+++ src/main/java/de/muspellheim/datenverteiler/datenkatalog/datenverteiler/Empfaengeranmeldung.java
@@ -10,7 +10,6 @@ import de.bsvrz.dav.daf.main.config.SystemObject;
 
 import java.util.Collection;
 import java.util.Objects;
-import java.util.function.Consumer;
 
 /**
  * Beschreibt eine einzelne Anmeldung als Empfänger für Datensätze vom Datenverteiler.
@@ -20,7 +19,6 @@ import java.util.function.Consumer;
  */
 public final class Empfaengeranmeldung<T> {
 
-    private Consumer<Datensatz<T>> empfaenger;
     private Collection<SystemObject> objekte;
     private Class<T> datumTyp;
     private Aspect aspekt;
@@ -29,17 +27,12 @@ public final class Empfaengeranmeldung<T> {
         // value object
     }
 
-    public static <T> Empfaengeranmeldung of(Consumer<Datensatz<T>> empfaenger, Collection<SystemObject> objekte, Class<T> datumTyp, Aspect aspekt) {
+    public static <T> Empfaengeranmeldung of(Collection<SystemObject> objekte, Class<T> datumTyp, Aspect aspekt) {
         Empfaengeranmeldung<T> result = new Empfaengeranmeldung<>();
-        result.empfaenger = empfaenger;
         result.objekte = objekte;
         result.datumTyp = datumTyp;
         result.aspekt = aspekt;
         return result;
-    }
-
-    public Consumer<Datensatz<T>> getEmpfaenger() {
-        return empfaenger;
     }
 
     public Collection<SystemObject> getObjekte() {
@@ -59,22 +52,20 @@ public final class Empfaengeranmeldung<T> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Empfaengeranmeldung that = (Empfaengeranmeldung) o;
-        return Objects.equals(empfaenger, that.empfaenger) &&
-                Objects.equals(objekte, that.objekte) &&
+        return Objects.equals(objekte, that.objekte) &&
                 Objects.equals(datumTyp, that.datumTyp) &&
                 Objects.equals(aspekt, that.aspekt);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(empfaenger, objekte, datumTyp, aspekt);
+        return Objects.hash(objekte, datumTyp, aspekt);
     }
 
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Empfaengeranmeldung{");
-        sb.append("empfaenger=").append(empfaenger);
-        sb.append(", objekte=").append(objekte);
+        sb.append("objekte=").append(objekte);
         sb.append(", datumTyp=").append(datumTyp);
         sb.append(", aspekt=").append(aspekt);
         sb.append('}');
