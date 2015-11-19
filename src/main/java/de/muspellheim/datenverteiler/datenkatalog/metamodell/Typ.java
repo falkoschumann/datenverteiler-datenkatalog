41de8b5e1dd3b4f2e9b566da351fe61a4e232aba
Falko Schumann <falko.schumann@muspellheim.de>
2015-11-19 23:15:14 +0100

    Megamodell für Typ, Supertypen und Mengen erstellt

---
+++ src/main/java/de/muspellheim/datenverteiler/datenkatalog/metamodell/Typ.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2015 Falko Schumann
+ * Released under the terms of the MIT License.
+ */
+
+package de.muspellheim.datenverteiler.datenkatalog.metamodell;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Der Typ aller Typobjekte.
+ *
+ * @author Falko Schumann
+ * @since 3.2
+ */
+public class Typ {
+
+    private Set<MengenVerwendung> mengen = new LinkedHashSet<>();
+    private Set<Typ> superTypen = new LinkedHashSet<>();
+
+    /**
+     * Führt die Mengen auf, die mit Objekten dieses Typs verwendet werden können oder müssen.
+     */
+    public Set<MengenVerwendung> getMengen() {
+        return mengen;
+    }
+
+    /**
+     * Jedem Typ ist eine Menge von Supertypen zugeordnet.
+     * <p>
+     * Supertypen sind die Typen, von dem der jeweilige Typ abgeleitet ist. Ein Typ erbt die Eigenschaften bezüglich der
+     * verwendbaren Attributgruppen und Mengen von all seinen Supertypen.
+     * </p>
+     */
+    public Set<Typ> getSuperTypen() {
+        return superTypen;
+    }
+
+}
