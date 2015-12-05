d5e636f29ecb95738eb40e67de112dd27b7205f5
Falko Schumann <falko.schumann@muspellheim.de>
2015-12-05 23:45:48 +0100

    HTML-Generator: Prototyp für Typ-Seite angelegt

--- src/main/java/de/muspellheim/datenverteiler/datenkatalog/generator/html/HtmlGenerator.java
+++ src/main/java/de/muspellheim/datenverteiler/datenkatalog/generator/html/HtmlGenerator.java
@@ -9,6 +9,7 @@ import de.bsvrz.puk.config.configFile.datamodel.ConfigDataModel;
 import de.muspellheim.datenverteiler.datenkatalog.metamodell.KonfigurationsBereich;
 import de.muspellheim.datenverteiler.datenkatalog.metamodell.Metamodell;
 import de.muspellheim.datenverteiler.datenkatalog.metamodell.SystemObjekt;
+import de.muspellheim.datenverteiler.datenkatalog.metamodell.Typ;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 
@@ -17,7 +18,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Collection;
-import java.util.Comparator;
+import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
@@ -29,6 +30,10 @@ import java.util.stream.Collectors;
  * @since 3.2
  */
 public class HtmlGenerator {
+
+    public static final String KONFIGURATIONSBEREICHE = "konfigurationsbereiche";
+    public static final String OBJEKTE = "objekte";
+    public static final String TYP = "typ";
 
     private final String source = "/generator/html/";
     private final String target = "target/datenkatalog-html/";
@@ -63,6 +68,9 @@ public class HtmlGenerator {
         generiereDatei("uebersicht");
         generiereDatei("uebersicht-frame");
         generiereDatei("alleobjekte-frame");
+        Set<SystemObjekt> objekte = (Set<SystemObjekt>) context.get(OBJEKTE);
+        for (SystemObjekt e : objekte)
+            generiereObjekt(e);
     }
 
     private VelocityContext erzeugeContext(Metamodell metamodell) {
@@ -70,11 +78,11 @@ public class HtmlGenerator {
 
         SortedSet<KonfigurationsBereich> konfigurationsBereiche = new TreeSet<>();
         konfigurationsBereiche.addAll(metamodell.getKonfigurationsbereiche());
-        result.put("konfigurationsbereiche", konfigurationsBereiche);
+        result.put(KONFIGURATIONSBEREICHE, konfigurationsBereiche);
 
         SortedSet<SystemObjekt> objekte = new TreeSet<>();
         objekte.addAll(metamodell.getKonfigurationsbereiche().stream().map(KonfigurationsBereich::getTypen).flatMap(Collection::stream).collect(Collectors.toSet()));
-        result.put("objekte", objekte);
+        result.put(OBJEKTE, objekte);
 
         return result;
     }
@@ -85,13 +93,26 @@ public class HtmlGenerator {
     }
 
     private void generiereDatei(String name) throws IOException {
-        OutputStream out = Files.newOutputStream(Paths.get(target, name + ".html"));
+        generiereDatei(name, name);
+    }
+
+    private void generiereDatei(String template, String zieldateiname) throws IOException {
+        OutputStream out = Files.newOutputStream(Paths.get(target, zieldateiname + ".html"));
         try (Writer writer = new OutputStreamWriter(out, "UTF-8")) {
             try {
-                Velocity.mergeTemplate(source + name + ".vm", "UTF-8", context, writer);
+                Velocity.mergeTemplate(source + template + ".vm", "UTF-8", context, writer);
             } catch (Exception ex) {
-                throw new IllegalStateException("Unreachable code. Fehler beim Erzeugen der Datei " + name + ".html.", ex);
+                throw new IllegalStateException("Unreachable code. Fehler beim Erzeugen der Datei " + zieldateiname + ".html mit dem Template " + template + ".vm.", ex);
             }
+        }
+    }
+
+    private void generiereObjekt(SystemObjekt systemObjekt) throws IOException {
+        if (systemObjekt instanceof Typ) {
+            context.put(TYP, systemObjekt);
+            String pfad = systemObjekt.getBereich().getZustaendiger().getPid() + "/" + systemObjekt.getBereich().getPid() + "/";
+            Files.createDirectories(Paths.get(target, pfad));
+            generiereDatei("typ", pfad + systemObjekt.getPid());
         }
     }
 
