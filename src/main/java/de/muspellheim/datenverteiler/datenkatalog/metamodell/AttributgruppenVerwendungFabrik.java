738596e75f06aa0df529b0be128f9cbed0362f76
Falko Schumann <falko.schumann@muspellheim.de>
2015-12-25 19:44:55 +0100

    Metamodell: Attributgruppenverwendung wird jetzt initialisiert

--- src/main/java/de/muspellheim/datenverteiler/datenkatalog/metamodell/AttributgruppenVerwendungFabrik.java
+++ src/main/java/de/muspellheim/datenverteiler/datenkatalog/metamodell/AttributgruppenVerwendungFabrik.java
@@ -5,6 +5,7 @@
 
 package de.muspellheim.datenverteiler.datenkatalog.metamodell;
 
+import de.bsvrz.dav.daf.main.config.AttributeGroupUsage;
 import de.bsvrz.dav.daf.main.config.SystemObject;
 
 /**
@@ -26,6 +27,38 @@ class AttributgruppenVerwendungFabrik extends SystemObjektFabrik<Attributgruppen
     @Override
     protected AttributgruppenVerwendung erzeugeObjekt(SystemObject objekt) {
         return AttributgruppenVerwendung.erzeugeMitPid(objekt.getPid());
+    }
+
+    @Override
+    protected void initialisiereObjekt(SystemObject object, AttributgruppenVerwendung result) {
+        super.initialisiereObjekt(object, result);
+
+        AttributeGroupUsage usage = (AttributeGroupUsage) object;
+        result.setAttributgruppe(getMetamodell().getAttributgruppe(usage.getAttributeGroup().getPid()));
+        result.setAspekt(getMetamodell().getAspekt(usage.getAspect().getPid()));
+        result.setVerwendungExplizitVorgegeben(usage.isExplicitDefined());
+        result.setDatensatzVerwendung(datensatzVerwendung(usage.getUsage()));
+    }
+
+    private DatensatzVerwendung datensatzVerwendung(AttributeGroupUsage.Usage usage) {
+        switch (usage.getId()) {
+            case 1:
+                return DatensatzVerwendung.KONFIGURIERENDER_DATENSATZ_NOTWENDIG;
+            case 2:
+                return DatensatzVerwendung.KONFIGURIERENDER_DATENSATZ_NOTWENDIG_UND_AENDERBAR;
+            case 3:
+                return DatensatzVerwendung.KONFIGURIERENDER_DATENSATZ_OPTIONAL;
+            case 4:
+                return DatensatzVerwendung.KONFIGURIERENDER_DATENSATZ_OPTIONALUND_AENDERBAR;
+            case 5:
+                return DatensatzVerwendung.ONLINE_DATENSATZ_QUELLE;
+            case 6:
+                return DatensatzVerwendung.ONLINE_DATENSATZ_SENKE;
+            case 7:
+                return DatensatzVerwendung.ONLINE_DATENSATZ_QUELLE_UND_SENKE;
+            default:
+                throw new IllegalStateException("Unbekannte Datensatzverwendung: " + usage);
+        }
     }
 
 }
